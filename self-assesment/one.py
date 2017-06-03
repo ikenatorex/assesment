70f3be52f526cd04e84ce8fff854ae5c3d7fa0e6
ikenatorex <ike.schober@gmail.com>
2017-06-02 17:58:02 -0700

    Update one.py

--- self-assesment/one.py
+++ self-assesment/one.py
@@ -1,1 +1,14 @@
 
+me = [
+    'My name is Michael',
+    'I am older than most of you',
+    'I like puppies',
+    'Yesterday I bought a truck'
+    ]
+
+
+def prints_paragraph(list_of_strings):
+    for string in list_of_strings:
+        print string
+        
+prints_paragraph(me)
